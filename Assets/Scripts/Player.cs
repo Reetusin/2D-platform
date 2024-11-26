56b4a1266e7f3b99755d4ec926ec4a7c71e7b666
Makilltas <matas.malis2@gmail.com>
2024-11-26 17:47:38 +0200

    2d platformer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement")]
+    public float speed = 10.0f;
+    public float jumpHeight = 3;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private float horizontal;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
+
+
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            var jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (groundCheck != null )
+        {
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
